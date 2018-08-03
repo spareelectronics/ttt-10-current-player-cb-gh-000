1e69488f6ff173b8b14dc9161d384db6d56eae83
Amin Ullah <aullah@umich.edu>
2018-08-03 06:59:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,11 @@
 def turn_count(board)
   counter = 0
   board.each do |position|
-    if (position == "X" || position == "O)
+    if (position == "X" || position == "O")
       counter += 1
     end
   end
 end
 
 
-def current_player(board)
-end
+
